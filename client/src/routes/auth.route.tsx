50cafaa64d015ab110d2915f972139330940565d
theprakashkumar <maiprakashkumar@gmail.com>
2025-07-22 16:11:25 +0530

    feat: Don't show login screen if user if logged in.

--- client/src/routes/auth.route.tsx
+++ client/src/routes/auth.route.tsx
@@ -1,7 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton";
+import useAuth from "@/hooks/api/use-auth";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { isAuthRoute } from "./common/routePaths";
 
 const AuthRoute = () => {
-  return <Outlet />;
+  const location = useLocation();
+  const { data: authData, isLoading } = useAuth();
+  const user = authData?.user;
+  // isAuthRoute given use the login routes.
+  const isLoginRoute = isAuthRoute(location.pathname);
+  // If login/signup route then don't show the loading screen.
+  if (isLoading && !isLoginRoute) return <DashboardSkeleton />;
+  if (!user) return <Outlet />;
+  return <Navigate to={`workspace/${user.currentWorkspace?._id}`} />;
 };
 
 export default AuthRoute;
