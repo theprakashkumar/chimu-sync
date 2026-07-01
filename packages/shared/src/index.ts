72de3f631fcd1e14769630ab69b15e101f974444
theprakashkumar <maiprakashkumar@gmail.com>
2026-07-01 07:44:49 +0530

    refactor: Removed constant folder from Shared library.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,4 @@
 
 export * from "./enums/task.enum";
 export * from "./enums/permission.enum";
-export * from "./constants";
-// export * from "./types/auth.types";
 export * from "./types/api.type";
